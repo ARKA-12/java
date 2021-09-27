b471bc2cecdb6b36515923cc24aa6017d642c6f9
ARKA-12 <64783542+ARKA-12@users.noreply.github.com>
2021-09-27 20:40:18 +0530

    Update MyFile.java

--- MyFile.java
+++ MyFile.java
@@ -3,8 +3,8 @@
 //    Step1: Compile the program written in java by using javac i.e (javac MyFile.java). 
 //    Step2: Run the program as (java MyFile) and you will get the output. The first few line of the program I import the necessary library required to make the program.
 
-// -> Logic: I had created an array of string and sorted it in descending order. Checking started from first index of array and if substring present in that String then it will replace it with null. And atlast if something present in that string then that string will be invalid and it will continue the array checking. 
-
+//    Logic: I had created an array of string and sorted it in descending order. Checking started from first index of array and if substring present in that String then it will replace it with null. And atlast if something present in that string then that string will be invalid and it will continue the array checking. 
+//    Approach: Bruteforce approach	
 
 
 
